bd369233d000f3ae267c137641b0c4f1a71a75b2
tropen <tropen09@gmail.com>
2021-09-01 16:06:42 +0300

    initial

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # node-todo-server
+
+1. `git checkout https://github.com/tropen/node-todo-server.git`
+2. copy .env-example
+3`npm start` start server
+4. `npm run start:dev` start server in development mode with nodemon for faster development process
---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict'
+
+// const express = require('express')
+// const dotenv = require('dotenv')
+// dotenv.config({ path: './.env' })
+//
+// const app = express()
+
+
+const http = require('http');
+
+const hostname  = '127.0.0.1';
+const port =  3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
